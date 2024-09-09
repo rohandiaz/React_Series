c440d57312eb450c5c9857bd298fe2826e4658c0
rohandiaz <rohanamylasedas@gmail.com>
2024-09-09 13:42:24 +0530

    React-Router

---
+++ 07_React Router/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/home.jsx'
+import Header from './Components/Header/header.jsx'
+import About from './Components/About/about.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+
+ 
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
